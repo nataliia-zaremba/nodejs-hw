f6c5827cb9554557f3b89a088db2f36b5ae4b822
Nataliia <natashazaremba82@gmail.com>
2025-10-16 20:51:11 +0300

    changes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -9,27 +9,27 @@ const router = express.Router();
 // Застосовуємо authenticate до всіх маршрутів нотаток
 router.use(authenticate);
 
-// GET /api/notes - Отримати всі нотатки користувача
+// Отримати всі нотатки
 router.get('/api/notes', notesController.getAllNotes);
 
-// GET /api/notes/:noteId - Отримати одну нотатку користувача
+// Отримати одну нотатку
 router.get('/api/notes/:noteId', notesController.getNoteById);
 
-// POST /api/notes - Створити нову нотатку
+// Створити нову
 router.post(
   '/api/notes',
   celebrate(notesValidationSchema),
   notesController.createNote,
 );
 
-// PATCH /api/notes/:noteId - Оновити нотатку користувача
+//  Оновити нотатку
 router.patch(
   '/api/notes/:noteId',
   celebrate(notesValidationSchema),
   notesController.updateNote,
 );
 
-// DELETE /api/notes/:noteId - Видалити нотатку користувача
+// Видалити нотатку
 router.delete('/api/notes/:noteId', notesController.deleteNote);
 
 export default router;
