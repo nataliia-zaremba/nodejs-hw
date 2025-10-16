c7f0329be7f200816d12f156f68a7e3712677a3c
Nataliia <natashazaremba82@gmail.com>
2025-10-16 21:10:05 +0300

    changes2

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,35 +1,35 @@
 import express from 'express';
-import { celebrate } from 'celebrate';
-import * as notesController from '../controllers/notesController.js';
-import { authenticate } from '../middleware/authenticate.js';
-import { notesValidationSchema } from '../validations/notesValidation.js';
+import {
+  noteIdSchema,
+  getAllNotesSchema,
+  createNoteSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  updateNote,
+  deleteNote,
+} from '../controllers/notesController.js';
 
 const router = express.Router();
 
-// Застосовуємо authenticate до всіх маршрутів нотаток
-router.use(authenticate);
+/* ---- Routes ---- */
 
 // Отримати всі нотатки
-router.get('/api/notes', notesController.getAllNotes);
+router.get('/', getAllNotesSchema, getAllNotes);
 
-// Отримати одну нотатку
-router.get('/api/notes/:noteId', notesController.getNoteById);
+// Отримати одну нотатку за ID
+router.get('/:noteId', noteIdSchema, getNoteById);
 
-// Створити нову
-router.post(
-  '/api/notes',
-  celebrate(notesValidationSchema),
-  notesController.createNote,
-);
+// Створити нову нотатку
+router.post('/', createNoteSchema, createNote);
 
-//  Оновити нотатку
-router.patch(
-  '/api/notes/:noteId',
-  celebrate(notesValidationSchema),
-  notesController.updateNote,
-);
+// Оновити нотатку
+router.patch('/:noteId', updateNoteSchema, updateNote);
 
 // Видалити нотатку
-router.delete('/api/notes/:noteId', notesController.deleteNote);
+router.delete('/:noteId', noteIdSchema, deleteNote);
 
 export default router;
